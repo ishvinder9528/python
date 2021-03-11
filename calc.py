9f9e9725f89ff309f7b5d06c487ab289fab49f53
Ishvinder Singh <ishvinder133@gmail.com>
2021-03-11 12:52:42 +0530

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,17 @@
+a=int(input("enter the first value:"))
+b=int(input("enter the second value:"))
+
+c=a+b
+print("the addition is:",c)
+
+c=a-b
+print("the subtraction is:",c)
+
+c=a*b
+print("the multiplication is:",c)
+
+c=a/b
+print("the division is:",c)
+
+c=a%b
+print("the percent is:",c)
