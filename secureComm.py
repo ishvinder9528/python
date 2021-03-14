a819d0123227f4b17e6f18f8362d71214ba8cc03
Ishvinder Singh <ishvinder133@gmail.com>
2021-03-14 15:22:40 +0530

    Update secureComm.py

--- secureComm.py
+++ secureComm.py
@@ -1,10 +1,14 @@
-k=int(input("enter the value of k for changing the text \nenter value b/w 1-20 : \n"))
+print("\n\n\n\n\n")
+print("HELLO MIT CELL")
+k=int(input("Please enter k value to shift transform message: "))
 if k<1 and k>20:
     print("enter the number b/w 1-20 only")
-str=input("enter your message : ")
+inp=input("Please enter your message : ")
 letter = 0
-print("Message after transformation is :")
-for i in str:
+ins=""
+
+print("You send :")
+for i in inp:
     if ord(i)>=97 and ord(i)<=122:                         #if all Lower char
         if ord(i)==122:                     #code for z
             letter = chr(90+(k-1))
@@ -22,10 +26,44 @@ for i in str:
             letter = 0+(k-1)
     elif i==" ":
         letter = "@"                        #for space
+    elif i==".":
+        letter = "#"                        #for .
     else:
         letter = i                          #for rest of symbols
     print(letter, end="")
+    
+    ins+=str(letter)                        #store the trasformed msg
 print("\nvalue of k is :",k)
 
+print()
+print()
+print()
 
 
+print("HELLO INSTRUCTOR:")
+print(f"YOU GOT A MESSAGE {ins}")
+p=int(input("To read this in original please type k value provided by Cell: "))
+print("ORIGIGNAL MESSAGE")
+for i in ins:
+    if ord(i)>=97 and ord(i)<=122:                         #if all Lower char
+        if ord(i)==97:                     #code for z
+            letter1 = chr(122-(p-1))
+        else:
+            letter1= chr(ord(i)-p)
+    elif ord(i)>=65 and ord(i)<=90:                       #if all Upper char
+        if ord(i)==65:                      #code for Z
+            letter1 = chr(65-(p-1))
+        else:
+            letter1= chr(ord(i)-p)
+    elif i.isalnum():                       #if all num
+        if int(i)>=1 and int(i)<=9:
+            letter1 = int(i)-p
+        else:
+            letter1 = 9-(p-1)
+    elif i=="@":
+        letter1 = " "                        #for space
+    elif i=="#":
+        letter1 = "."                        #for .
+    else:
+        letter1 = i                          #for rest of symbols
+    print(letter1, end="")
